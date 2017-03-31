18ac304367f522f94fc7f1de02393f3661f67d36
Mokhet <mokhet@mokhet.com>
2017-03-31 21:51:37 +0200

    Fix hulkbuster uniform

--- js/CHARACTERS.js
+++ js/CHARACTERS.js
@@ -40,7 +40,7 @@ MFF.CHARACTERS =
                                    })
         };
 },
-"getNameForUniform" : function(character, uniform) { return MFF.CHARACTERS.DATA[character].uniforms[uniform].name; },
+"getNameForUniform" : function(character, uniform) { return (character in MFF.CHARACTERS.DATA) && (uniform in MFF.CHARACTERS.DATA[character].uniforms) ? MFF.CHARACTERS.DATA[character].uniforms[uniform].name : character; },
 "getImageUrlForUniform" : function(character, uniform) { return "images/characters/{0}/{1}.png".format(uniform, character); },
 "getImageForUniform" : function(character, uniform)
 {
@@ -90,7 +90,8 @@ MFF.CHARACTERS =
  var i, name;
  if ( !(id in MFF.CHARACTERS.DATA) ) { return "The character \"{0}\" is not a known character".format(id); }
  if ( !data.uniform ) { return "The uniform of the character \"{0}\" is not defined".format(id); }
- if ( !(data.uniform in MFF.CHARACTERS.DATA[id].uniforms) ) { return "The uniform \"{0}\" of the character \"{1}\" is not a known uniform".format(data.uniform, id); }
+ if ( id == "hulkbuster" && data.uniform == "avengers" ) { data.uniform = "aaou"; } // fix data error corrected in 2.3
+ // if ( !(data.uniform in MFF.CHARACTERS.DATA[id].uniforms) ) { return "The uniform \"{0}\" of the character \"{1}\" is not a known uniform".format(data.uniform, id); }
  name = MFF.CHARACTERS.getNameForUniform(id, data.uniform);
  if ( !("gear" in data) ) { return "Gears for the character \"{0}\" are not defined".format(name); }
  if ( !Array.isArray(data.gear) || data.gear.length != 4 ) { return "Gears for the character \"{0}\" is not a valid array of 4 items".format(name); }
@@ -118,6 +119,7 @@ MFF.CHARACTERS =
    if ( !("physical" in all[k].defense) ) { all[k].defense.pysical = 0; }
    if ( !("energy" in all[k].defense) ) { all[k].defense.energy = 0; }
    if ( !("skills" in all[k]) || !Array.isArray(all[k].skills) || all[k].skills.length != 5 ) { all[k].skills = [0, 0, 0, 0, 0]; }
+   if ( k == "hulkbuster" && all[k].uniform == "avengers" ) { all[k].uniform = "aaou"; } // fix data error corrected in 2.3
    if ( "uniforms" in all[k] )
    {
     for ( kk in all[k].uniforms )
