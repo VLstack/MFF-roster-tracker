94106d459bb86b4113219379207ecb57ae512c38
Mokhet <mokhet@mokhet.com>
2017-04-21 19:36:48 +0200

    Fix #45

--- js/polyfill.js
+++ js/polyfill.js
@@ -19,6 +19,31 @@ Array.isArray = Array.isArray || function(arg)
  return Object.prototype.toString.call(arg) === "[object Array]";
 };
 
+if ( !Array.prototype.find )
+{
+ Object.defineProperty(Array.prototype,
+                       "find",
+                       {
+                        "value" : function(predicate)
+                        {
+                         var o, len, thisArg, k, kValue;
+                         if ( this == null ) { throw new TypeError("\"this\" is null or not defined"); }
+                         o = Object(this);
+                         len = o.length >>> 0;
+                         if ( typeof predicate !== "function" ) { throw new TypeError("predicate must be a function"); }
+                         thisArg = arguments[1];
+                         k = 0;
+                         while ( k < len )
+                         {
+                          kValue = o[k];
+                          if ( predicate.call(thisArg, kValue, k, o) ) { return kValue; }
+                          k++;
+                         }
+                         return undefined;
+                        }
+                       });
+}
+
 /* STRING */
 
 if ( !String.prototype.format )
